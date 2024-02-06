26b882d1054236f3f80ddcc913b2a4423dad4817
Damian <damianpietron007@gmail.com>
2024-02-06 02:17:01 +0100

    Add navbar and jinja2 integration

--- taskmate/todo_list/views.py
+++ taskmate/todo_list/views.py
@@ -1,6 +1,4 @@
-from django.http import HttpResponse
 from django.shortcuts import render
 
-# Create your views here.
 def todo_list(request):
-    return render(request, 'todo_list.html', {})
+    return render(request, 'todo_list.html', {'welcome_txt': "Welcome to Taskmate"})
