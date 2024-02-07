d4d1a0058b7df10ad9841a61024c4dac34dcc684
Damian <damianpietron007@gmail.com>
2024-02-07 14:46:11 +0100

    Add forms and messages

--- taskmate/todo_list_app/views.py
+++ taskmate/todo_list_app/views.py
@@ -1,15 +1,24 @@
-from django.shortcuts import render
+import django.contrib.messages
+from django.shortcuts import render, redirect
 from .models import Task
-
+from .forms import TaskForm
+from django.contrib import messages
 def todo_list(request):
-
-    all_data = Task.objects.all
-    content = {
-        'todo_content': all_data
-    }
-    return render(
-        request, 'todo_list.html', content
-    )
+    if request.method == 'POST':
+        form = TaskForm(request.POST or None)
+        if form.is_valid():
+            print('form is valid')
+            form.save()
+        messages.success(request, ('New Task Added!'))
+        return redirect('todo_list')
+    else:
+        all_data = Task.objects.all
+        content = {
+            'todo_content': all_data
+        }
+        return render(
+            request, 'todo_list.html', content
+        )
 
 def contact(request):
     content = {
