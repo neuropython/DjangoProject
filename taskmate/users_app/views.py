acaf9c56b0236bb098d14c3b99b6b5a73a7a12db
DamianPietron <128989743+neuropython@users.noreply.github.com>
2024-02-11 20:35:52 +0100

    Debug register form saving data

--- taskmate/users_app/views.py
+++ taskmate/users_app/views.py
@@ -8,6 +8,7 @@ def register(request):
         register_form = CustomUserCreationForm(request.POST)
 
         if register_form.is_valid():
+            register_form.save()
             messages.success(request, ("New User Created, Login to get started"))
             return redirect('register')
     else:
